7da4d8f9bedf86e244e1fcd2147770ecb478a5b3
Apharaohh <ocka56990@gmail.com>
2026-05-29 23:29:13 -0700

    Updating code

--- main.tf
+++ main.tf
@@ -2,20 +2,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+resource "aws_s3_bucket" "app_server" {
 
   tags = {
     Name = "learn-terraform"
