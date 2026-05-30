c57771d77a72ff275b01cdbee8d7a038833d89a2
Apharaohh <ocka56990@gmail.com>
2026-05-29 23:36:33 -0700

    Week 1: S3 bucket with Terraform

--- main.tf
+++ main.tf
@@ -2,9 +2,20 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_s3_bucket" "app_server" {
+resource "aws_s3_bucket" "app_bucket" {
+  bucket = "app-server-learn-terraform-12345"  # Must be globally unique
 
   tags = {
     Name = "learn-terraform"
   }
+}
+
+output "bucket_name" {
+  value       = aws_s3_bucket.app_bucket.id
+  description = "Name of the S3 bucket"
+}
+
+output "bucket_arn" {
+  value       = aws_s3_bucket.app_bucket.arn
+  description = "ARN of the S3 bucket"
 }
